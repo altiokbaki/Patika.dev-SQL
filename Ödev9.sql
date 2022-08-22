14d7f1aaeb980fbf02da33f52f9f3fe3f52b813a
Osman Baki ALTIOK <osmanbakialtiok@gmail.com>
2022-08-22 04:29:16 +0300

    Ödev9 yanıtları yüklendi.

---
+++ Ödev9.sql
@@ -0,0 +1,11 @@
+www.patika.dev SQL // Ödev9
+
+SELECT city AS cities , country.country AS countries FROM city
+INNER JOIN country ON city.country_id = country.country_id
+--WHERE country.country = 'Turkey';
+
+SELECT payment_id , first_name , last_name FROM customer
+INNER JOIN payment ON customer.customer_id = payment.customer_id
+
+SELECT rental_id , first_name , last_name FROM rental
+INNER JOIN customer ON rental.customer_id = customer.customer_id;
